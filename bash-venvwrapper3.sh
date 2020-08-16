1524331c1a1a0d060fbda9a1edf2234af8b3a3be
David Pascual Rocher <dapasroc@gmail.com>
2020-08-16 11:04:22 +0200

    Create bash-venvwrapper3.sh
    Virtual env wrapper functionality and completions for bash.

---
+++ bash-venvwrapper3.sh
@@ -0,0 +1,38 @@
+#/usr/bin/env bash
+
+VENVS_HOME="$HOME/.virtualenvs3"
+
+if [ ! -d "$VENVS_HOME" ]; then
+    echo "Home of virtualenvs does not exists, creating it"
+    mkdir $VENVS_HOME
+fi
+
+workon() {
+    if [ -z "$1" ]; then
+        echo "No virtualenv specified"
+        exit 2
+    fi
+
+    source $VENVS_HOME/$1/bin/activate
+}
+
+mkvirtualenv() {
+    if [ -z "$1" ]; then
+        echo "No new virtualenv name specified, you must specify one"
+        exit 2
+    fi
+
+    python3 -m venv $HOME/.virtualenvs3/$1
+}
+
+lsvirtualenvs() {
+    ls $VENVS_HOME
+}
+
+_workon_completions() {
+    DIR_LIST=$(ls $VENVS_HOME)
+
+    COMPREPLY=($(compgen -W "$DIR_LIST" "${COMP_WORDS[1]}"))
+}
+
+complete -F _workon_completions workon
