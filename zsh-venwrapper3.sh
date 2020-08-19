8a9337b8ced00dd7d9f522457e4fe801e5f31c49
David Pascual Rocher <dapasroc@gmail.com>
2020-08-19 07:26:53 +0200

    Make zsh version commands more like to the bash version

--- zsh-venwrapper3.sh
+++ zsh-venwrapper3.sh
@@ -2,19 +2,48 @@
 VENVS_HOME="HOME/.virtualenvs3/"
 
 function workon () { 
- source $HOME/.virtualenvs3/$1/bin/activate }
-function mkvenv3 () { python3 -m venv $HOME/.virtualenvs3/$1 }
-function mkvenv37 () { python3.7 -m venv $HOME/.virtualenvs3/$1 }
-function lsvenv3 () { ls $HOME/.virtualenvs3 }
-function rmvenv3 () { rm -rf $HOME/.virtualenvs3/$1 }
+    if [ -z "$1" ]; then
+        echo "No virtualenv specified, you must specify one."
+        echo "Usage: "
+        echo "\t workon venv_name"
+        exit 1
+    fi
 
-function _workon3 {
+    source $HOME/.virtualenvs3/$1/bin/activate
+}
+
+function mkvirtualenv () {
+    if [ -z "$1" ]; then
+        echo "No new virtualenv name specified, you must specify one."
+        echo "Usage:"
+        echo "\t mkvirtualenv new_venv_name"
+        exit 1
+    fi
+    
+    python3 -m venv $HOME/.virtualenvs3/$1
+}
+
+function lsvirtualenvs () { 
+    ls $HOME/.virtualenvs3
+}
+function rmvirtualenv () {
+    if [ -z "$1" ]; then
+        echo "No virtualenv specified to be deleted."
+        echo "Usage:"
+        echo "\t rmvirtualenv venv_name"
+        exit 1
+    fi
+    
+    rm -rf $HOME/.virtualenvs3/$1
+}
+
+function _workon {
     _values 'virtualenvs' "${(f)$(ls ~/.virtualenvs3/)}"
 }
 
-function _rmvenv3 {
+function _rmvirtualenv {
     _values 'virtualenvs' "${(f)$(ls ~/.virtualenvs3/)}"
 }
 
-compdef _workon3 workon3
-compdef _rmvenv3 rmvenv3
+compdef _workon workon
+compdef _rmvirtualenv rmvirtualenv
